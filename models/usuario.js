f5fcea712bfdbf135306efe4d23ba595a0b985a8
Juan Guillermo Rozo <juanrozo@MacBook-Pro-de-Juan.local>
2019-07-03 08:40:38 -0500

    ajuste

--- models/usuario.js
+++ models/usuario.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 const { ObjectId } = schema.Types;
 
+var estadosValidos = {
+  values: ['APROBADO', 'RECHAZADO'],
+  message: '{VALUE} no es un rol permitido'
+};
 
 
 var usuarioSchema = new schema({
@@ -9,7 +13,9 @@ var usuarioSchema = new schema({
   correo: {type: String, unique:[true, 'El correo debe ser unico'], required: [true, 'El correo es necesario']},
   clave: {type: String, required: [true, 'La clave es necesaria']},
   cedula: { type: String,unique:[true, 'El correo debe ser unico'], required: [true, 'La cedula es obligatorio']},
-  creditos: [{type: ObjectId, ref: 'Credito'}]
+  creditos: [{type: ObjectId, ref: 'Credito'}],
+  estadoDeCredito: {type: String, enum: estadosValidos, default: 'APROBADO', required: false},
+
 
 
 
--- routes/credito.js
+++ routes/credito.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var Credito = require("../models/credito");
+var Usuario = require("../models/usuario");
 var fs = require("fs");
 var app = express();
 var mdUsuario = require("../middlewares/usuario");
@@ -67,6 +68,75 @@ app.put("/credito/:id", (req, res) => {
       res.status(200).json({
         ok: true,
         credito: creditoGuardado
+      });
+    });
+  });
+});
+
+// =============================
+// RECHAZAR CREDITO
+// =============================
+
+app.put("/credito/:id/:id_usuario", (req, res) => {
+  var id = req.params.id;
+  var id_usuario = req.params.id_usuario;
+
+  Credito.findById(id, (err, credito) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al buscar credito",
+        error: err
+      });
+    }
+
+    if (!credito) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "El credito con el id" + id + "no existe",
+        error: "No existe un credito con ese ID"
+      });
+    }
+
+    credito.estadoDeCredito = 'RECHAZADO';
+    
+    credito.save((err, creditoGuardado) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          mensaje: "Error al actualizar credito",
+          error: err
+        });
+      }
+      Usuario.findById(id_usuario,  (err, usuario) => {
+        if (err) {
+          return res.status(500).json({
+            ok: false,
+            mensaje: "Error al buscar usuario",
+            error: err
+          });
+        }
+
+        if (!usuario) {
+          return res.status(400).json({
+            ok: false,
+            mensaje: "El usuario con el id" + id + "no existe",
+            error: "No existe un usuario con ese ID"
+          });
+        }
+        usuario.estadoDeCredito = 'RECHAZADO';
+        usuario.save((err, usuarioGuardado) => {
+          if (err) {
+            return res.status(500).json({
+              ok: false,
+              mensaje: "Error al actualizar usuario",
+              error: err
+            });
+          }
+
+      res.status(200).json({
+        ok: true,
+        credito: usuarioGuardado
       });
     });
   });
